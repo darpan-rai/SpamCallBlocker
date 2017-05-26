ec763d86b05e1afe492d3b7856f5d4a7d2e0fc24
D.R <darpan.rai@email.com>
2017-05-26 06:37:26 +0100

    Create Storage.java

---
+++ Storage.java
@@ -0,0 +1,21 @@
+package com.darpan.scba;
+
+import android.database.sqlite.*;
+/**
+ * Created by Darpan on 27-Mar-16.
+ *
+ * This class handles the search database, add number to database, and monthly update function of the
+ * local database function.
+ */
+
+public final class Storage
+{
+    //empty constructor to prevent someone from accidentally instantiating the class.
+    public Storage()
+    {
+
+    }
+
+
+
+}
